23d50ca2d804625e3511eb1c8f215c5f15516e14
Cory Sanders <corysanders3@yahoo.com>
2024-06-26 18:45:43 -0500

    add images to public folder

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -5,12 +5,12 @@ function Nav() {
     return (
         <nav className='nav-container'>
             <div>
-                <h1>Cory Sanders</h1>
+                <h1 className='nav-header'>Cory Sanders</h1>
             </div>
             <div className='nav-buttons'>
-                <a href='https://www.linkedin.com/in/corysanders3/' className='nav-button'>LinkedIn</a>
-                <a href='https://github.com/corysanders3' className='nav-button'>GitHub</a>
-                <a href='https://drive.google.com/file/d/1ivzryjZakDKHhFEistfuRA9_yRyrA89-/view?usp=sharing' className='nav-button'>Resume</a>
+                <a href='https://www.linkedin.com/in/corysanders3/' target='_blank' className='nav-button'>LinkedIn</a>
+                <a href='https://github.com/corysanders3' target='_blank' className='nav-button'>GitHub</a>
+                <a href='https://drive.google.com/file/d/19FMnxY-ADBdl-q_NKgkd8d8HBI7R_NTg/view?usp=sharing' target='_blank' className='nav-button'>Resume</a>
             </div>
         </nav>
     )
